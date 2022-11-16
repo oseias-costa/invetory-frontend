c9023c96105d55fc52b08e47c2dac71416cb3b61
unknown <oseiasc2@gmail.com>
2022-11-16 17:32:48 -0300

    Update handleEditCategory function

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { useEffect, useState } from 'react';
 
 function App() {
   const [category, setCategory] = useState([])
-  const [newCategory, setNewCategory] = useState({})
+  const [newCategory, setNewCategory] = useState({categoryName:''})
   const [editCategory, setEditCategory] = useState(false)
     
   useEffect(() => {
@@ -15,47 +15,47 @@ function App() {
  const getData = () => axios.get('/api/category').then(res => {
     const dados = res.data
     setCategory(dados)
-  })
+  }).catch(err => console.log(err))
+
+  const deleteFromList = (id, arr) => {
+    const indexCategory = arr.findIndex(item => item._id === id)
+      arr.splice(indexCategory, 1)
+  }
      
   const handleDelete = (item) => {
-    const del = item.target.value
-    axios.delete(`/api/category/${del}`).then(() => {
-      const indexCategory = category.findIndex(item => item._id === del)
-      category.splice(indexCategory, 1)
+    const id = item.target.value
+    axios.delete(`/api/category/${id}`).then(() => {
+      deleteFromList(id, category)
       setCategory([...category])
     }).catch(err => console.log(err))
   }
 
   const handleSave = () => {
-    if(newCategory !== ''){
+    if(newCategory.categoryName !== ''){
       axios.post('/api/category',{
         categoryName: newCategory.categoryName
       }).then( response => setCategory([...category, response.data]))
       .catch(err => console.log(err))
     }
+    setNewCategory({})
   }
 
   const handleEdit = () => {
-    console.log(newCategory)
-    // if(newCategory._id !== '') return
-    axios.patch(`/api/category/${newCategory._id}`,{
+     if(newCategory._id !== '') {
+      axios.patch(`/api/category/${newCategory._id}`,{
       categoryName: newCategory.categoryName
     }).then(response => {
-      console.log(response.data)
-      setCategory([response.data, ...category])
+        deleteFromList(newCategory._id, category)
+        setCategory([response.data, ...category])
       if(response.ok){
         category.sort((a, b) => new Date(b.updatedAt) - new Date(a.updatedAt))
       }
-      
+      setNewCategory({categoryName: ''})
     }).then(setCategory(category.sort((a, b) => new Date(b.updatedAt) - new Date(a.updatedAt))))
-    .catch(err => console.log(err))
+    .catch(err => console.log(err))}
     setNewCategory({})
     setEditCategory(false)
   }
-
-  useEffect(() => {
-    category.sort((a, b) => new Date(b.updatedAt) - new Date(a.updatedAt))
-  }, [editCategory])
 
   console.log('ORDEMMM', category.sort((a, b) => new Date(b.updatedAt) - new Date(a.updatedAt)))
   const handleEditCategory = (item) => {
@@ -84,7 +84,7 @@ function App() {
           Learn React
         </a>
       </header>
-      <input type='text' value={newCategory.categoryName} onChange={e => setNewCategory({...newCategory,['categoryName'] : e.target.value})} />
+      <input type='text' required={+true} value={newCategory.categoryName} onChange={e => setNewCategory({...newCategory, categoryName : e.target.value})} />
       <button onClick={editCategory ? handleEdit : handleSave}>{editCategory ? 'Editar' : 'Salvar'}</button>
       
 
