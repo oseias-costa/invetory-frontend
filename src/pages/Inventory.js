3f6633034d76657888e77660f14e7fbd3fd40639
Oséias Costa <oseiasc2@gmail.com>
2022-11-24 14:14:17 +0000

    test terminal codesandbox

--- src/pages/Inventory.js
+++ src/pages/Inventory.js
@@ -1,37 +1,59 @@
-import { MdChecklistRtl } from "react-icons/md"
-import { Section } from "../styles/global/components/Section"
-import { TopPage } from "../styles/global/components/TopPage"
+import { MdChecklistRtl } from "react-icons/md";
+import { Section } from "../styles/global/components/Section";
+import { TopPage } from "../styles/global/components/TopPage";
 import { useContext, useEffect, useState } from "react";
 import { ProductContext } from "../context/ProductContext";
 import { TableInventory } from "../components/TableInventory";
-import { AddInventoryItem, InventoryItem } from "../components/AddInventoryItem";
-import { NavLink, Route, Router, Routes } from "react-router-dom";
+import { AddInventoryItem } from "../components/AddInventoryItem";
+import { NavLink, Route, Routes } from "react-router-dom";
 import { EditInventoryItem } from "../components/EditInventoryItem";
 
 export const Inventory = () => {
-    const { inventory, setInventory } = useContext(ProductContext)  
-    const [ inventorySelected, setInventorySelected] = useState({})
-    const [ selectedItem, setselectedItem] = useState()
+  const { inventory, setInventory } = useContext(ProductContext);
+  const [inventorySelected, setInventorySelected] = useState({});
+  const [selectedItem, setselectedItem] = useState();
 
-    return(
-        <Section>
-            <TopPage>
-                <MdChecklistRtl />
-                <h2>Estoque</h2>
-            </TopPage>
-            <NavLink to='/Estoque/'>Estoque</NavLink>
-            <NavLink to='/Estoque/Adicionar'>Adicionar</NavLink>
-            <NavLink to='/Estoque/Editar'>Editar</NavLink>
-                <Routes>
-                    <Route path='/' element={
-                        <TableInventory inventory={inventory} selectedItem={selectedItem} setselectedItem={setselectedItem} />} />
-                    <Route path='/Adicionar' element={
-                        <AddInventoryItem setInventory={setInventory} inventory={inventory} />} />
-                    <Route path='/Editar' element={
-                        <EditInventoryItem selectedItem={selectedItem} inventory={inventory} setselectedItem={setselectedItem} />} />
-                    <Route path='/Movimentar' />
-                </Routes>  
-            
-        </Section>
-    )
-}
+  return (
+    <Section>
+      <TopPage>
+        <MdChecklistRtl />
+        <h2>Estoque</h2>
+      </TopPage>
+      <NavLink to="/Estoque/">Estoque</NavLink>
+      <NavLink to="/Estoque/Adicionar">Adicionar</NavLink>
+      <NavLink to="/Estoque/Editar">Editar</NavLink>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <TableInventory
+              inventory={inventory}
+              selectedItem={selectedItem}
+              setselectedItem={setselectedItem}
+            />
+          }
+        />
+        <Route
+          path="/Adicionar"
+          element={
+            <AddInventoryItem
+              setInventory={setInventory}
+              inventory={inventory}
+            />
+          }
+        />
+        <Route
+          path="/Editar"
+          element={
+            <EditInventoryItem
+              selectedItem={selectedItem}
+              inventory={inventory}
+              setselectedItem={setselectedItem}
+            />
+          }
+        />
+        <Route path="/Movimentar" />
+      </Routes>
+    </Section>
+  );
+};
